0c1c20da96b7ad72318ef55b53b6f0ac73d311c2
Colin Courtney <ccour004@gmail.com>
2020-05-12 08:58:12 -0400

    Adding file list.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import { GoogleLogin } from 'react-google-login';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Card from '@material-ui/core/Card';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import './App.css';
 
-function App() {
-  console.log("TEST SECRET: "+process.env.REACT_APP_TEST_SECRET)
-  return (
-    <div className="App">
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state={files:[]}
+  }
+
+  loginSuccess = (returnObj) => {
+    fetch(`https://www.googleapis.com/drive/v3/files?key=${process.env.REACT_APP_CLIENT_ID}`,
+      {headers: {
+        "Authorization": `Bearer ${returnObj.accessToken}`,
+        "Accept": "application/json"
+      }})
+      .then(response => response.json())
+      .then(data => this.setState({files:data.files}))
+  }
+
+  render(){
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <GoogleLogin
           clientId={process.env.REACT_APP_CLIENT_ID}
-          onSuccess={()=>alert("Login sucess!")}
+          onSuccess={(returnObj)=>this.loginSuccess(returnObj)}
           onFailure={(error,details)=>alert("Login failure: "+JSON.stringify(error)+"("+details+")")}
+          accessType="offline"
+          scope="https://www.googleapis.com/auth/drive.metadata.readonly"
           cookiePolicy={'single_host_origin'}
           isSignedIn={true}
         />
+        {this.state.files !== []?
+          <div>
+          <GoogleLogout
+            clientId={process.env.REACT_APP_CLIENT_ID}
+            onLogoutSuccess={()=>this.setState({files:[]})}
+            onFailure={(error,details)=>alert("Logout failure: "+JSON.stringify(error)+"("+details+")")}
+          />
+          <List>
+            {this.state.files.map((file)=>{
+              return <Card><ListItem><ListItemText primary={file.name} secondary={file.kind} /></ListItem></Card>
+            })
+            }
+          </List>
+          </div>
+        :<div/>}
       </header>
-    </div>
-  );
+      </div> 
+    )
+  }
 }
 
 export default App;
